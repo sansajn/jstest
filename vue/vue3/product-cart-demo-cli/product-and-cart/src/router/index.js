5fcb766aed8333839d48b022f0e611b4ebf18046
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2023-06-23 11:54:35 +0200

    project without linter (can't manage dependencies)

--- vue/vue3/product-cart-demo-cli/product-and-cart/src/router/index.js
+++ vue/vue3/product-cart-demo-cli/product-and-cart/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProductsView from '../views/ProductsView.vue'
 
 const routes = [
   {
@@ -9,9 +8,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/products',
-    name: 'Products',
-    component: ProductsView
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
