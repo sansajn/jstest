dafaf0a1f8c3e80198c08b2e96b2749813424b37
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2023-06-23 12:32:45 +0200

    partial work

--- vue/vue3/product-cart-demo-cli/product-and-cart/src/router/index.js
+++ vue/vue3/product-cart-demo-cli/product-and-cart/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
 import PastOrders from '../views/PastOrders.vue'
 
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/products',
---
+++ vue/vue3/product-cart-demo-cli/product-and-cart/src/views/Home.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="home">
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <div v-for="(product, idx) in inventory.slice(0,3)" :key="product.id" class="card">
+          <div class="card-title">
+            {{ product.name }}
+          </div>
+          <div class="card-body">
+            <i class="icofont-10x icofont-{{ product.icon }}"></i>
+            <form>
+              <div class="row">
+                <div class="cell">
+                  <label>Type:</label>
+                </div>
+                <div class="cell">
+                  <em>{{ product.type }}</em>
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Price:</label>
+                </div>
+                <div class="cell">
+                  ${{ product.price.USD }}
+                </div>
+              </div>
+              <div class="row">
+                <div class="cell">
+                  <label>Quantity:</label>
+                </div>
+                <div class="cell">
+                  <input type="number" v-model.number="product.quantity">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-footer">
+            <button @click="addToCard(product.name, idx)" class="btn btn-light">
+              Add to cart
+            </button>
+          </div>
+        </div>
+        
+      </div>
+
+    </main>
+
+  </div>
+</template>
+
+<script>
+import food from '../food.json'
+
+export default {
+  name: 'Home',
+  
+  data() {
+    return {
+      inventory: food
+    }
+  },
+
+  components: {
+  }
+}
+</script>
